8d563da90100fb6abd4e8f52686943faa2b36341
ELCOT <ELCOT@Lenovo>
2022-05-14 15:23:40 +0530

    first commit

---
+++ PreparedStatementJDBCProject/src/main/java/com/java/MainClass.java
@@ -0,0 +1,44 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		while(true) {
+		System.out.println("Database operations");
+		System.out.println("Enter your choice");
+		System.out.println("1.To display Student information");
+		System.out.println("2.Insert Student record");
+		System.out.println("3.Update Student information based on id");
+		System.out.println("4.Delete Student information based on id");
+		System.out.println("5.Select Student based on ID");
+		int ch=sc.nextInt();
+		
+		switch(ch) {
+		case 1:DataBaseOperations.displayRecords();
+		        break;
+		case 2: DataBaseOperations.addRecords();
+		      break;
+		case 3: DataBaseOperations.updateRecords();
+		      break;
+		case 4: DataBaseOperations.deleteRecords();
+		break;
+		case 5: DataBaseOperations.selectRecords();
+		      break;
+		 default:System.out.println("Invalid choice");
+		} //switch
+		
+		System.out.println("Do you want to continue yes/no");
+		String choice=sc.next();
+		if(choice.equalsIgnoreCase("no")) {
+			break;
+		} //if
+		
+		}//while end
+		System.out.println("Program Terminated");
+		
+	}
+
+}
